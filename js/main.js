5a5064d9944c83bdf75d1446dce99fc3d62a92aa
GioGio4 <g.ceccobelli@outlook.it>
2023-02-11 19:05:24 +0100

    Add Vue code

--- js/main.js
+++ js/main.js
@@ -1,147 +1,183 @@
-// Milestone 0:
-// Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo
-// l'immagine grande in modo da poter stilare lo slider.
+// Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
+// Bonus:
+// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+// ----------------------------------------------------------------------------------------------------------------------------//
 
-// Milestone 1:
-// Ora rimuoviamo i contenuti statici e usiamo l'array di oggetti letterali per popolare dinamicamente il carosello. Al click dell'utente
-// sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+const { createApp } = Vue;
 
-// Milestone 2:
-// Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra,
-// la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
-
-// BONUS 1:
-// Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l'immagine corrispondente.
-
-// BONUS 2:
-// Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l'immagine attiva dovrà cambiare alla successiva.
-
-// BONUS 3:
-// Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
-
-const slides = [
-  {
-    image: "img/01.webp",
-    title: "Marvel's Spiderman Miles Morale",
-    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+createApp({
+  data() {
+    return {
+      slides: {
+        images: [
+          "img/01.webp",
+          "img/02.webp",
+          "img/03.webp",
+          "img/04.webp",
+          "img/05.webp",
+        ],
+        titles: [
+          "Marvel's Spiderman Miles Morale",
+          "Ratchet & Clank: Rift Apart",
+          "Fortnite",
+          "Stray",
+          "Marvel's Avengers",
+        ],
+        text: [
+          "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+          "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+          "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+          "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+          "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+        ],
+      },
+      activeSlides: 0,
+    };
   },
-  {
-    image: "img/02.webp",
-    title: "Ratchet & Clank: Rift Apart",
-    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+  methods: {
+    prevImage() {
+      if (this.activeSlides - 1 < 0) {
+        this.activeSlides = this.slides.images.length - 1;
+      } else {
+        this.activeSlides--;
+      }
+    },
+    nextImage() {
+      if (this.activeSlides + 1 >= this.slides.images.length) {
+        this.activeSlides = 0;
+      } else {
+        this.activeSlides++;
+      }
+    },
   },
-  {
-    image: "img/03.webp",
-    title: "Fortnite",
-    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-  },
-  {
-    image: "img/04.webp",
-    title: "Stray",
-    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
-  },
-  {
-    image: "img/05.webp",
-    title: "Marvel's Avengers",
-    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-  },
-];
+}).mount("#app");
 
-// * RECUPERO GLI ELEMENTI HTML
-const slidesContainerEl = document.getElementById("slides-container");
-const thumbsContainerEl = document.getElementById("thumbs-container");
-const prevButton = document.getElementById("prev");
-const nextButton = document.getElementById("next");
-const stopAutoplayButton = document.getElementById("stop-autoplay");
-const invertiAutoplayButton = document.getElementById("inverti-autoplay");
+// ------------------------------------------------------------------------------------------------------------------//
+// const slides = [
+//   {
+//     image: "img/01.webp",
+//     title: "Marvel's Spiderman Miles Morale",
+//     text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+//   },
+//   {
+//     image: "img/02.webp",
+//     title: "Ratchet & Clank: Rift Apart",
+//     text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+//   },
+//   {
+//     image: "img/03.webp",
+//     title: "Fortnite",
+//     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+//   },
+//   {
+//     image: "img/04.webp",
+//     title: "Stray",
+//     text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+//   },
+//   {
+//     image: "img/05.webp",
+//     title: "Marvel's Avengers",
+//     text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+//   },
+// ];
 
-// * SETTO IL "CONTATORE" DELL'IMMAGINE ATTIVA
-let activeImage = 0;
+// // * RECUPERO GLI ELEMENTI HTML
+// const slidesContainerEl = document.getElementById("slides-container");
+// const thumbsContainerEl = document.getElementById("thumbs-container");
+// const prevButton = document.getElementById("prev");
+// const nextButton = document.getElementById("next");
+// const stopAutoplayButton = document.getElementById("stop-autoplay");
+// const invertiAutoplayButton = document.getElementById("inverti-autoplay");
 
-// * GENERO SLIDES E THUMBNAILS
-slides.forEach((slide, index) => {
-  slidesContainerEl.innerHTML += `
-    <div class="slide ${index == activeImage ? "active" : ""}">
-        <img src="./${slide.image}">
-        <div class="slide-text">
-            <h2>${slide.title}</h2>
-            <p>${slide.text}</p>
-        </div>
-    </div>`;
+// // * SETTO IL "CONTATORE" DELL'IMMAGINE ATTIVA
+// let activeImage = 0;
 
-  thumbsContainerEl.innerHTML += `
-    <div class="thumb" data-index="${index}">
-        <img src="./${slide.image}">
-    </div>
-    `;
-});
+// // * GENERO SLIDES E THUMBNAILS
+// slides.forEach((slide, index) => {
+//   slidesContainerEl.innerHTML += `
+//     <div class="slide ${index == activeImage ? "active" : ""}">
+//         <img src="./${slide.image}">
+//         <div class="slide-text">
+//             <h2>${slide.title}</h2>
+//             <p>${slide.text}</p>
+//         </div>
+//     </div>`;
 
-// * LEGO IL CLICK DELLE THUMBNAILS AL CAMBIO DELL'IMMAGINE VISUALIZZATA
-const thumbsEl = document.querySelectorAll(".thumb");
-thumbsEl.forEach((thumbEl, index) => {
-  thumbEl.addEventListener("click", function () {
-    const thisIndex = this.getAttribute("data-index");
-    switchToSlide(thisIndex);
-  });
-});
+//   thumbsContainerEl.innerHTML += `
+//     <div class="thumb" data-index="${index}">
+//         <img src="./${slide.image}">
+//     </div>
+//     `;
+// });
 
-// * CREO UNA FUNZIONE PER GESTIRE L'AVANZAMENTO DEL CAROSELLO
-const onNextClick = () => {
-  activeImage++;
+// // * LEGO IL CLICK DELLE THUMBNAILS AL CAMBIO DELL'IMMAGINE VISUALIZZATA
+// const thumbsEl = document.querySelectorAll(".thumb");
+// thumbsEl.forEach((thumbEl, index) => {
+//   thumbEl.addEventListener("click", function () {
+//     const thisIndex = this.getAttribute("data-index");
+//     switchToSlide(thisIndex);
+//   });
+// });
 
-  if (activeImage >= slides.length) {
-    activeImage = 0;
-  }
+// // * CREO UNA FUNZIONE PER GESTIRE L'AVANZAMENTO DEL CAROSELLO
+// const onNextClick = () => {
+//   activeImage++;
 
-  // console.log(activeImage);
+//   if (activeImage >= slides.length) {
+//     activeImage = 0;
+//   }
 
-  switchToSlide(activeImage);
-};
+//   // console.log(activeImage);
 
-// * CREO UNA FUNZIONE PER GESTIRE L'ARRETRAMENTO DEL CAROSELLO
-const onPrevClick = () => {
-  activeImage--;
+//   switchToSlide(activeImage);
+// };
 
-  if (activeImage < 0) {
-    activeImage = slides.length - 1;
-  }
+// // * CREO UNA FUNZIONE PER GESTIRE L'ARRETRAMENTO DEL CAROSELLO
+// const onPrevClick = () => {
+//   activeImage--;
 
-  // console.log(activeImage);
+//   if (activeImage < 0) {
+//     activeImage = slides.length - 1;
+//   }
 
-  switchToSlide(activeImage);
-};
+//   // console.log(activeImage);
 
-// * CREO UNA FUNZIONE PER CAMBIARE L'IMMAGINE VISUALIZZATA
-const switchToSlide = (activeIndex) => {
-  const activeSlide = document.querySelector(".slide.active");
-  const allSlides = document.querySelectorAll(".slide");
+//   switchToSlide(activeImage);
+// };
 
-  activeSlide.classList.remove("active");
-  allSlides[activeIndex].classList.add("active");
-};
+// // * CREO UNA FUNZIONE PER CAMBIARE L'IMMAGINE VISUALIZZATA
+// const switchToSlide = (activeIndex) => {
+//   const activeSlide = document.querySelector(".slide.active");
+//   const allSlides = document.querySelectorAll(".slide");
 
-// * LEGO IL CLICK DEI CONTROLLI ALLE FUNZIONI DI AVANZAMENTO E ARRETRAMENTO
-nextButton.addEventListener("click", onNextClick);
-prevButton.addEventListener("click", onPrevClick);
+//   activeSlide.classList.remove("active");
+//   allSlides[activeIndex].classList.add("active");
+// };
 
-// * AUTOPLAY
-let autoplayForward = true;
+// // * LEGO IL CLICK DEI CONTROLLI ALLE FUNZIONI DI AVANZAMENTO E ARRETRAMENTO
+// nextButton.addEventListener("click", onNextClick);
+// prevButton.addEventListener("click", onPrevClick);
 
-const autoplay = setInterval(() => {
-  if (autoplayForward) {
-    onNextClick();
-  } else {
-    onPrevClick();
-  }
-}, 3000);
+// // * AUTOPLAY
+// let autoplayForward = true;
 
-// * STOP AUTOPLAY
-stopAutoplayButton.addEventListener("click", () => {
-  clearInterval(autoplay);
-});
+// const autoplay = setInterval(() => {
+//   if (autoplayForward) {
+//     onNextClick();
+//   } else {
+//     onPrevClick();
+//   }
+// }, 3000);
 
-// * INVERTI AUTOPLAY
+// // * STOP AUTOPLAY
+// stopAutoplayButton.addEventListener("click", () => {
+//   clearInterval(autoplay);
+// });
 
-invertiAutoplayButton.addEventListener("click", () => {
-  autoplayForward = !autoplayForward;
-});
+// // * INVERTI AUTOPLAY
+
+// invertiAutoplayButton.addEventListener("click", () => {
+//   autoplayForward = !autoplayForward;
+// });
